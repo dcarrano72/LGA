7fbb1c8aba0b95dbb10bfdf44e857402b52c60be
David Carrano <davidcarrano@Macs-MacBook-Pro.local>
2019-08-08 11:55:16 -0400

    lgm update crud

--- controllers/lgsessionController.js
+++ controllers/lgsessionController.js
@@ -8,15 +8,15 @@ const LGSession = require('../models/lgsessionschema.js');
 const seedData = require('../models/seed.js');
 
 // seed route
-lgsession.get('/seed', (req, res) => {
-    LGSession.create(seedData, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('index.ejs')
-        }
-    })
-})
+// lgsession.get('/seed', (req, res) => {
+//     LGSession.create(seedData, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.render('index.ejs')
+//         }
+//     })
+// })
 
 // index route displays start button
 lgsession.get('/', (req, res) => {
@@ -26,14 +26,43 @@ lgsession.get('/', (req, res) => {
 // new session 
 lgsession.get('/new_session', (req, res) => {
     res.render('new_session.ejs')
-    // autogenerate lgsession id 
-    // display form enter lg id
-    // display button to active session
 })
 
 lgsession.get('/active_session/:id', (req, res) => {
-    res.render('active_session.ejs', {
-        sessionId: req.params.id
+    LGSession.findById(req.params.id, (err, currentSession) => {
+        const something = {
+            business_name: "",
+            business_contact: "",
+            business_phone: "",
+            business_email: "",
+            website_url: "",
+            note: ""
+        }
+        res.render('active_session.ejs', {
+            sessionId: req.params.id,
+            currentSession: currentSession,
+            currentLead: something
+        })
+    })
+    // display session id
+    // display lg id
+    // display form to enter new lead
+})
+
+lgsession.get('/active_session/:sessionId/:leadId', (req, res) => {
+    LGSession.findById(req.params.sessionId, (err, currentSession) => {
+        let something = {};
+
+        currentSession.leads.forEach((lead) => {
+            if (lead._id == req.params.leadId) {
+                something = lead;
+            }
+        })
+        res.render('active_session.ejs', {
+            sessionId: req.params.id,
+            currentSession: currentSession,
+            currentLead: something
+        })
     })
     // display session id
     // display lg id
@@ -50,10 +79,10 @@ lgsession.post('/make_new_session', (req, res) => {
     })
 })
 
-lgsession.get('/new', (req, res) => {
-    res.render('new.ejs')
-    // add new lead to current lgsession in database
-})
+// lgsession.get('/new', (req, res) => {
+//     res.render('new.ejs')
+//     // add new lead to current lgsession in database
+// })
 
 lgsession.put('/add_new_lead/:id', (req, res) => {
     LGSession.findByIdAndUpdate(req.params.id, { $push: { leads: req.body } }, (err, addedLead) => {
@@ -63,23 +92,83 @@ lgsession.put('/add_new_lead/:id', (req, res) => {
 
 
 
-
-lgsession.put('/active_session/:id', (req, res) => {
-    res.redirect('/active_session')
+// Update Lead
+lgsession.put('/update_lead/:sessionId/:leadId', (req, res) => {
+    LGSession.findOneAndUpdate({ 'leads._id': req.params.leadId }, {
+        '$set': {
+            'leads.$.business_name': req.body.business_name,
+            'leads.$.business_contact': req.body.business_contact,
+            'leads.$.business_phone': req.body.business_phone,
+            'leads.$.business_email': req.body.business_email,
+            'leads.$.website_url': req.body.website_url,
+            'leads.$.note': req.body.note
+        }
+    }, (err, foundSession) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(foundSession)
+            res.redirect(`/lgsession/active_session/${req.params.sessionId}`)
+        }
+    })
 })
 
-lgsession.delete('/active_session/:id', (req, res) => {
-    LGSession.findById(req.params.id, (err, item) => {
+// Edit
+lgsession.get('/edit/:sessionId/:leadId', (req, res) => {
+    LGSession.findById(req.params.sessionId, (err, foundSession) => {
+        if (err) {
+            console.log('DB error', err)
+        } else {
+            console.log('successfully found', foundSession)
+
+            let temp = {};
+
+            foundSession.leads.forEach((lead, index) => {
+                if (lead._id == req.params.leadId) {
+                    temp = lead;
+                }
+            })
+
+            res.render('edit.ejs', {
+                specificLead: temp
+                // sessionId: req.params.sessionId
+            })
+        }
+    })
+})
+
+lgsession.put('/active_session/:sessionId/:leadId', (req, res) => {
+    LGSession.findByIdAndUpdate(req.params.sessionId, { $pull: { leads: { _id: req.params.leadId } } }, (err, activeSession) => {
         if (err) {
             console.log(err)
 
         }
-        console.log('successfully removed', item)
-        item.remove()
+        console.log('successfully removed', req.params.leadId)
         res.redirect('/lgsession/show_leads')
     })
-
 })
+
+lgsession.delete('/active_session/:sessionId/:leadId', (req, res) => {
+    LGSession.findByIdAndUpdate(req.params.sessionId, { $pull: { leads: { _id: req.params.leadId } } }, (err, activeSession) => {
+        if (err) {
+            console.log(err)
+
+        }
+        console.log('successfully removed', req.params.leadId)
+        res.redirect('/lgsession/show_leads')
+    })
+})
+
+lgsession.delete('/delete_session/:sessionId', (req, res) => {
+    LGSession.findOneAndDelete(req.params.sessionId, (err, deletedSession) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('successfully removed session', req.params.sessionId)
+        res.redirect('/lgsession/show_leads')
+    })
+})
+
 
 lgsession.get('/show_leads', (req, res) => {
     LGSession.find({}, (err, foundSession) => {
